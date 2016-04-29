67112411770cb131fd577cf41f7febc496fead75
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2016-04-29 13:49:04 +0100

    Fix private methods on terminal width calculator

--- lib/kitcat/terminal_width_calculator.rb
+++ lib/kitcat/terminal_width_calculator.rb
@@ -1,35 +1,37 @@
 module TerminalWidthCalculator
-  def self.calculate
-    default_width = 80
+  class << self
+    def calculate
+      default_width = 80
 
-    term_width = calculate_term_width
+      term_width = calculate_term_width
 
-    term_width > 0 ? term_width : default_width
-  end
-
-  private
-
-  def self.calculate_term_width
-    if ENV['COLUMNS'] =~ /^\d+$/
-      ENV['COLUMNS'].to_i
-    elsif tput_case?
-      `tput cols`.to_i
-    elsif stty_case?
-      `stty size`.scan(/\d+/).map { |s| s.to_i }[1]
+      term_width > 0 ? term_width : default_width
     end
-  rescue
-    0
-  end
 
-  def self.tput_case?
-    (RUBY_PLATFORM =~ /java/ || !STDIN.tty? && ENV['TERM']) && shell_command_exists?('tput')
-  end
+    private
 
-  def self.stty_case?
-    STDIN.tty? && shell_command_exists?('stty')
-  end
+    def calculate_term_width
+      if ENV['COLUMNS'] =~ /^\d+$/
+        ENV['COLUMNS'].to_i
+      elsif tput_case?
+        `tput cols`.to_i
+      elsif stty_case?
+        `stty size`.scan(/\d+/).map { |s| s.to_i }[1]
+      end
+    rescue
+      0
+    end
 
-  def self.shell_command_exists?(command)
-    ENV['PATH'].split(File::PATH_SEPARATOR).any?{|d| File.exists? File.join(d, command) }
+    def tput_case?
+      (RUBY_PLATFORM =~ /java/ || !STDIN.tty? && ENV['TERM']) && shell_command_exists?('tput')
+    end
+
+    def stty_case?
+      STDIN.tty? && shell_command_exists?('stty')
+    end
+
+    def shell_command_exists?(command)
+      ENV['PATH'].split(File::PATH_SEPARATOR).any?{|d| File.exists? File.join(d, command) }
+    end
   end
 end
